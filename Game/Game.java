c14a1e8bf70644cb3c502fd33fae8bfb382b830f
UkraineBolt <alawrence2211@gmail.com>
2019-05-05 20:26:20 -0400

    UniCastProtocol is done and file paths reworked so project can run in my intellij

--- Game/Game.java
+++ Game/Game.java
@@ -3,11 +3,11 @@ package Game;
 import java.util.LinkedList;
 
 public class Game {
-    Deck deck;
+    /*Deck deck;
     LinkedList<Player> players;
     public Game(LinkedList<Player> players){
         this.players = players;
         deck = new Deck();
-    }
+    }*/
 
 }
---
+++ Game/Game/Client/UniCastProtocol.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game.Client;
+
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import Game.Shared.Constants;
+
+/**
+ *
+ * @author alawren3
+ */
+public class UniCastProtocol {
+    private static Constants C;
+    private static DatagramSocket socket;
+    private static DatagramPacket receiver;
+    private static DatagramPacket sender;
+
+    UniCastProtocol(int port, String address, int timeOutInMill){
+        try {
+            socket = new DatagramSocket();
+            socket.setSoTimeout(timeOutInMill);
+            sender = new DatagramPacket(new byte[C.MAXDATASIZE],C.MAXDATASIZE, InetAddress.getByName(address),port);
+            receiver = new DatagramPacket(new byte[C.MAXDATASIZE],C.MAXDATASIZE);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void send(byte[] data){
+        sender.setData(data);sender.setPort(data.length);
+        try {
+            socket.send(sender);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /*notes
+    using InterruptedIOException to signify that you didnt get the packet you wanted
+    in the amount of time you expected.
+     */
+    public byte[] recieve(int attempts) throws InterruptedIOException {
+        int count = 0;
+
+        try {
+            socket.receive(receiver);
+            byte[] data = new byte[receiver.getLength()];
+            ByteBuffer bb = ByteBuffer.wrap(receiver.getData());
+            bb.get(data);
+            return data;
+        } catch(SocketTimeoutException e){
+            count++;
+            if (count>=attempts){
+                throw new InterruptedIOException();
+            }
+            return null;
+        }catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+    
+}
