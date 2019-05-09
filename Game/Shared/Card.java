fc999900b1c269c79bf749e330b3f37264712eb5
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-08 22:46:03 -0400

    changed card to implement serializable

--- Game/Shared/Card.java
+++ Game/Shared/Card.java
@@ -1,26 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Game.Shared;
 
-public class Card {
-    CardType value;
-    Suit suit;
+import java.io.Serializable;
+
+public class Card implements Serializable {
+    private CardType value;
+    private Suit suit;
 
     public Card(CardType value, Suit suit) {
         this.value = value;
         this.suit = suit;
     }
 
-    public static boolean checkMove(Card topCard, Card newCard){
+    public Suit getSuit(){
+        return suit;
+    }
+
+    public CardType getCardVal(){
+        return value;
+    }
+
+    public void setSuit(Suit s){
+        suit = s;
+    }
+
+    public static boolean checkMove(Card topCard, Card newCard) {
         //if wild card then move is always valid
         //if the new cards suit or value matches the top cards then move is valid
         //else move is false
-        if(newCard.value == CardType.Wild || newCard.value == CardType.WildDraw)
+        if (newCard.getCardVal() == CardType.Wild || newCard.getCardVal() == CardType.WildDraw)
             return true;
-        else if(newCard.value == topCard.value || newCard.suit == topCard.suit)
+        else if (newCard.getCardVal() == topCard.getCardVal() || newCard.getSuit() == topCard.getSuit())
             return true;
         else
             return false;
@@ -28,6 +37,6 @@ public class Card {
 
     @Override
     public String toString(){
-        return "Card: " + value.toString() + " Suit: " + suit.toString();
+        return suit.toString() + " " + value.toString();
     }
 }
